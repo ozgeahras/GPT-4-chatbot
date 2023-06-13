7ec7ec67f7c3b853d290fc9c432dbb93e26be0e9
ozgeahras <ozgeahras@gmail.com>
2023-06-13 21:50:40 +0300

    updated openai api key path

--- openaiConfig.js
+++ openaiConfig.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   organization: "org-8sBBKnnzpa0m1QQdIgYJtvhS",
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  apiKey: process.env.VITE_OPENAI_API_KEY,
 });
 delete configuration.baseOptions.headers["User-Agent"];
 const openai = new OpenAIApi(configuration);
